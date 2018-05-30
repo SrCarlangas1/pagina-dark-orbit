f2f2e5d3cfa6a955db22abe294f3879567a74738
SrCarlangas1 <32309069+SrCarlangas1@users.noreply.github.com>
2018-05-30 14:41:08 +0200

    Add files via upload

--- js/javas.js
+++ js/javas.js
@@ -19,22 +19,24 @@ function cargarnoticias(json) {
     var data = '<div class="row noticias">';
     $.each(json, function (i, parte) {
         data+=
-        	'<div class="col-sm-4 fondo">' + 
-            '<br>' +
-            '<p>' + "DATE:" + parte.date + '</p>' +
-        	'<img src="' + parte.img + '"class="img-circle img-responsive" id="imgnoticia1">' + 
+        	'<div class="col-sm-4">' + 
+            '<div class="thumbnail">'+
+            '<p class="text-left">' + "DATE:" + parte.date + '</p>' +
+        	'<img src="' + parte.img + '"class="img-responsive">' + 
             '<h1>' + parte.titulo + '</h1>' +
+            '<div class="thumbnail">'+
             '<p>' + parte.descripcion + '</p>' +
+            '</div>'+
+            '</div>'+
             '</div>';
     }); 
     data+="</div>";
-    $("#contenedor").append(data);
+    $("#principal").append(data);
 };
 
 $(window).scroll(function () {
     if ($(window).scrollTop() + $(window).height() + 10 >= $(document).height()) {
         if (numero < 3) {
-            $("#contenedor").append('<div id="noticias" class="row">');
 			$.getJSON("https://rawgit.com/SrCarlangas1/pagina-dark-orbit/master/json/" + numero + ".json", function (jsonObject) {
             cargarnoticias(jsonObject);
             numero++;
